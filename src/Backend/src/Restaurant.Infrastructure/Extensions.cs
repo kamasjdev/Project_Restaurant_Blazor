f1d706ec4e0fd46865f999ae6b5b5c7905fcd381
KW <k.wojtasinski@wp.pl>
2022-11-25 17:34:26 +0100

    Restaurant: Backend Swagger
    Added packages
    Configure Swagger

--- src/Backend/src/Restaurant.Infrastructure/Extensions.cs
+++ src/Backend/src/Restaurant.Infrastructure/Extensions.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 using Restaurant.Infrastructure.Repositories;
 
 namespace Restaurant.Infrastructure
@@ -10,6 +11,15 @@ namespace Restaurant.Infrastructure
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddControllers();
+            services.AddSwaggerGen(swagger =>
+            {
+                swagger.CustomSchemaIds(s => s.FullName);
+                swagger.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Restaurant API",
+                    Version = "v1"
+                });
+            });
             services.Configure<AppOptions>(configuration.GetRequiredSection("app"));
             services.AddInMemoryRepositories();
             return services;
@@ -17,6 +27,13 @@ namespace Restaurant.Infrastructure
 
         public static WebApplication UseInfrastructure(this WebApplication app)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(swaggerUI =>
+            {
+                swaggerUI.RoutePrefix = "swagger";
+                swaggerUI.SwaggerEndpoint("/swagger/v1/swagger.json", "Restaurant API v1");
+                swaggerUI.DocumentTitle = "Restaurant API";
+            });
             app.UseAuthorization();
             app.MapControllers();
             return app;
