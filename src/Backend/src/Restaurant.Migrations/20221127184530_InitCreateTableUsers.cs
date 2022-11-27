9e1cf95751050db4a8f8f6287af68cf5a925fc4a
KW <k.wojtasinski@wp.pl>
2022-11-27 18:53:19 +0100

    Restaurant: Backend Migarations
    Added Users table

---
+++ src/Backend/src/Restaurant.Migrations/20221127184530_InitCreateTableUsers.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+
+namespace Restaurant.Migrations
+{
+    [Migration(20221127184530)]
+    public class InitCreateTableUsers : Migration
+    {
+        public override void Down()
+        {
+            Delete.Table("users");
+        }
+
+        public override void Up()
+        {
+            Create.Table("users")
+                .WithColumn("Id").AsGuid().PrimaryKey()
+                .WithColumn("Email").AsString(5000).NotNullable().Unique("uidx_users_email")
+                .WithColumn("Password").AsString(50).NotNullable()
+                .WithColumn("Role").AsString(50).NotNullable().Indexed("idx_users_role")
+                .WithColumn("CreatedAt").AsDateTime().NotNullable().Indexed("idx_users_created_at");
+        }
+    }
+}
