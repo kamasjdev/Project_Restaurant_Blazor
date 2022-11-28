281e7f47ea3391f19c18fb833fc2eea68d852c93
KW <k.wojtasinski@wp.pl>
2022-11-28 22:55:07 +0100

    Restaurant: Backend gRPC
    Configure API Cors
    Added gRPC Server

--- src/Backend/src/Restaurant.Infrastructure/Extensions.cs
+++ src/Backend/src/Restaurant.Infrastructure/Extensions.cs
@@ -4,12 +4,15 @@ using Microsoft.Extensions.DependencyInjection;
 using Restaurant.Infrastructure.Database;
 using Restaurant.Infrastructure.Documentation;
 using Restaurant.Infrastructure.Exceptions;
+using Restaurant.Infrastructure.Grpc.Services;
 using Restaurant.Infrastructure.Time;
 
 namespace Restaurant.Infrastructure
 {
     public static class Extensions
     {
+        private const string CorsPolicy = "CorsPolicy";
+
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddDocs();
@@ -17,15 +20,23 @@ namespace Restaurant.Infrastructure
             services.AddErrorHandling();
             services.AddDatabase(configuration);
             services.AddTime();
+            services.AddGrpc();
             services.Configure<AppOptions>(configuration.GetRequiredSection("app"));
+            services.AddCors(cors => cors.AddPolicy(CorsPolicy, options =>
+            {
+                options.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod();
+            }));
             return services;
         }
 
         public static WebApplication UseInfrastructure(this WebApplication app)
         {
+            app.UseCors(CorsPolicy);
             app.UseDocs();
             app.UseErrorHandling();
             app.UseAuthorization();
+            app.UseGrpcWeb();
+            app.MapGrpcService<WeatherService>().EnableGrpcWeb();
             app.MapControllers();
             return app;
         }
---
+++ src/Backend/src/Restaurant.Infrastructure/Grpc/Services/WeatherService.cs
@@ -0,0 +1,29 @@
+﻿using Restaurant.Shared;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+
+namespace Restaurant.Infrastructure.Grpc.Services
+{
+    public class WeatherService : WeatherForecasts.WeatherForecastsBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public override Task<WeatherReply> GetWeather(WeatherForecast request, ServerCallContext context)
+        {
+            var reply = new WeatherReply();
+            var rng = new Random();
+
+            reply.Forecasts.Add(Enumerable.Range(1, 10).Select(index => new WeatherForecast
+            {
+                Date = Timestamp.FromDateTime(DateTime.UtcNow.AddDays(index)),
+                TemperatureC = rng.Next(20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            }));
+
+            return Task.FromResult(reply);
+        }
+    }
+}
