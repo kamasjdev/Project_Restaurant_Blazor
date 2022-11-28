0028cab82b7a995c16af0c62d85360a996d58540
KW <k.wojtasinski@wp.pl>
2022-11-28 22:59:31 +0100

    Restaurant: Frontend Blazor gRPC
    Created Blazor project
    Setup gRPC

---
+++ src/Frontend/Restaurant.UI/Program.cs
@@ -0,0 +1,22 @@
+using Restaurant.Shared;
+using Grpc.Net.Client.Web;
+using Grpc.Net.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Restaurant.UI;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton(services =>
+{
+    var httpClient = new HttpClient(new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler()));
+    //var baseUri = services.GetRequiredService<NavigationManager>().BaseUri;
+    var baseUri = "http://localhost:5000"; // TODO add from appsettings
+    var channel = GrpcChannel.ForAddress(baseUri, new GrpcChannelOptions { HttpClient = httpClient });
+    return new WeatherForecasts.WeatherForecastsClient(channel);
+});
+
+await builder.Build().RunAsync();
