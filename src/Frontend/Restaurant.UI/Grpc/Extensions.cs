f535247ea0b2c9288425d3889d3a20a8040173b5
KW <k.wojtasinski@wp.pl>
2022-11-30 19:45:21 +0100

    Restaurant: Frontend Blazor Appsettings, Configuration
    Added appsettings
    Configure gRPC Clients

---
+++ src/Frontend/Restaurant.UI/Grpc/Extensions.cs
@@ -0,0 +1,68 @@
+﻿using Grpc.Net.Client.Web;
+using Grpc.Net.Client;
+using Restaurant.Shared.AdditionProto;
+using Restaurant.Shared;
+using Restaurant.Shared.OrderProto;
+using Restaurant.Shared.ProductProto;
+using Restaurant.Shared.ProductSaleProto;
+using Restaurant.Shared.UserProto;
+
+namespace Restaurant.UI.Grpc
+{
+	internal static class Extensions
+	{
+		public static IServiceCollection AddGrpcClients(this IServiceCollection services, IConfiguration configuration)
+		{
+			var backendUrl = configuration["externalClients:backend"];
+
+			if (backendUrl is null)
+			{
+				throw new InvalidOperationException("Invalid backend url");
+			}
+
+			services.AddSingleton(services =>
+			{
+				var httpClient = new HttpClient(new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler()));
+				var baseUri = backendUrl;
+				var channel = GrpcChannel.ForAddress(baseUri, new GrpcChannelOptions { HttpClient = httpClient });
+				return new WeatherForecasts.WeatherForecastsClient(channel);
+			});
+			services.AddSingleton(services =>
+			{
+				var httpClient = new HttpClient(new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler()));
+				var baseUri = backendUrl;
+				var channel = GrpcChannel.ForAddress(baseUri, new GrpcChannelOptions { HttpClient = httpClient });
+				return new Additions.AdditionsClient(channel);
+			});
+			services.AddSingleton(services =>
+			{
+				var httpClient = new HttpClient(new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler()));
+				var baseUri = backendUrl;
+				var channel = GrpcChannel.ForAddress(baseUri, new GrpcChannelOptions { HttpClient = httpClient });
+				return new Orders.OrdersClient(channel);
+			});
+			services.AddSingleton(services =>
+			{
+				var httpClient = new HttpClient(new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler()));
+				var baseUri = backendUrl;
+				var channel = GrpcChannel.ForAddress(baseUri, new GrpcChannelOptions { HttpClient = httpClient });
+				return new Products.ProductsClient(channel);
+			});
+			services.AddSingleton(services =>
+			{
+				var httpClient = new HttpClient(new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler()));
+				var baseUri = backendUrl;
+				var channel = GrpcChannel.ForAddress(baseUri, new GrpcChannelOptions { HttpClient = httpClient });
+				return new ProductSales.ProductSalesClient(channel);
+			});
+			services.AddSingleton(services =>
+			{
+				var httpClient = new HttpClient(new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler()));
+				var baseUri = backendUrl;
+				var channel = GrpcChannel.ForAddress(baseUri, new GrpcChannelOptions { HttpClient = httpClient });
+				return new Users.UsersClient(channel);
+			});
+			return services;
+		}
+	}
+}
