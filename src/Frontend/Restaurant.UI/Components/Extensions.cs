956aa18970626a11c6b3baf517033c7f540e251b
KW <k.wojtasinski@wp.pl>
2022-12-02 23:44:21 +0100

    Restaurant: Frontend Improvements
    Added FormProperty and Extension methods
    Cleanup TableComponent

---
+++ src/Frontend/Restaurant.UI/Components/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components.Rendering;
+
+namespace Restaurant.UI.Components
+{
+    public static class Extensions
+    {
+        public static void AddAttributeIfNotNullOrEmpty(this RenderTreeBuilder builder, int sequence, string name, string? value)
+        {
+            if (!string.IsNullOrEmpty(value))
+            {
+                builder.AddAttribute(sequence, name, value);
+            }
+        }
+    }
+}
---
+++ src/Frontend/Restaurant.UI/Components/FormProperty.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Restaurant.UI.Components
+{
+    public class FormProperty<T>
+    {
+        [NotNull] public T Value { get; set; }
+        public string? Error { get; set; }
+        public IEnumerable<Func<T, string>>? Rules { get; set; } = new List<Func<T, string>>();
+
+        public bool IsValid()
+        {
+            foreach(var rule in Rules)
+            {
+                var text = rule.Invoke(Value);
+                
+                if (!string.IsNullOrWhiteSpace(text))
+                {
+                    Error = text;
+                    return false;
+                }
+            }
+
+            Error = "";
+            return true;
+        }
+    }
+}
